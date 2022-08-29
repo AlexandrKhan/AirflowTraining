d257a5a9077170a7b9958826edb3b6c43f98b25e
Aliaksandr-Khan <Aliaksandr_Khan@epam.com>
2022-08-29 15:43:12 +0400

    #4 Great_Expectations (local checkpoint works, ge docker image)

--- great/great_expectations/plugins/expectations/expect_column_values_to_start_with_vowel.py
+++ great/great_expectations/plugins/expectations/expect_column_values_to_start_with_vowel.py
@@ -11,22 +11,22 @@ from great_expectations.expectations.metrics import (ColumnMapMetricProvider,
 
 class CustomSqlDataset(ColumnMapMetricProvider):
     condition_metric_name = "expect_column_values_to_start_with_vowel"
+    condition_value_keys = ("start",)
 
     @column_condition_partial(engine=SqlAlchemyExecutionEngine)
-    def _sqlalchemy(cls, column, _dialect, **kwargs):
-        vowels = ["a", "e", "i", "o", "u", "y"]
-        return column in vowels
+    def _sqlalchemy(cls, column, start, **kwargs):
+        return column.startswith(start)
 
 
 class ExpectColumnValuesToStartWithVowel(ColumnMapExpectation):
     """
-    Expect the column value to start with a vowel ('a', 'e', 'i', 'o', 'u', 'y')
+    Expect the column value to start with substring ("start" :parameter)
     """
 
     examples = [
         {
             "data": {
-                "all_vowels": ["animal", "old", "yikes"],
+                "all_vowels": ["oakland", "oak", "oasis"],
                 "not_all_vowels": ["big", "bad", "wolf"],
             },
             "tests": [
@@ -34,7 +34,11 @@ class ExpectColumnValuesToStartWithVowel(ColumnMapExpectation):
                     "title": "basic_positive_test",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "all_vowels", "mostly": 0.8},
+                    "in": {
+                        "column": "all_vowels",
+                        "start": "oa",
+                        "mostly": 0.8
+                    },
                     "out": {
                         "success": True,
                     },
@@ -43,7 +47,11 @@ class ExpectColumnValuesToStartWithVowel(ColumnMapExpectation):
                     "title": "basic_negative_test",
                     "exact_match_out": False,
                     "include_in_gallery": True,
-                    "in": {"column": "not_all_vowels"},
+                    "in": {
+                        "column": "not_all_vowels",
+                        "start": "oa",
+                        "mostly": 0.8
+                    },
                     "out": {
                         "success": False,
                     },
@@ -52,19 +60,17 @@ class ExpectColumnValuesToStartWithVowel(ColumnMapExpectation):
             "test_backends": [
                 {
                     "backend": "sqlalchemy",
-                    "dialects": ["postgresql"],
+                    "dialects": ["sqlite", "postgresql"],
                 },
             ],
         }
     ]
 
     map_metric = "expect_column_values_to_start_with_vowel"
-
-    # This is a list of parameter names that can affect whether the Expectation evaluates to True or False
-    success_keys = ("mostly",)
-
-    # This dictionary contains default values for any parameters that should have default values
-    default_kwarg_values = {}
+    success_keys = ("mostly", "start")
+    default_kwarg_values = {
+        "start": "oa"
+    }
 
     def validate_configuration(
         self, configuration: Optional[ExpectationConfiguration]
